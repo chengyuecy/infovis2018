3e35936dabe53a88e38dba9b3dd9ff8e4627f6e5
chengyuecy <38451501+chengyuecy@users.noreply.github.com>
2018-05-01 13:59:27 +0900

    Update main01.js

--- W08/main01.js
+++ W08/main01.js
@@ -30,13 +30,14 @@ function main()
     var faces = [
         [ 0, 1, 2 ], // f0
     ];
-    var scalars = [
-        0.1, // S0
-        0.2, // S1
-        0.8 // S2
-];
 
-    // Create a color map
+    var scalars = [
+        0.1,   // S0
+        0.2, // S1
+        0.8  // S2
+    ];
+
+    // Create color map
     var cmap = [];
     for ( var i = 0; i < 256; i++ )
     {
@@ -48,7 +49,7 @@ function main()
         cmap.push( [ S, '0x' + color.getHexString() ] );
     }
 
-    // Draw the color map
+    // Draw color map
     var lut = new THREE.Lut( 'rainbow', cmap.length );
     lut.addColorMap( 'mycolormap', cmap );
     lut.changeColorMap( 'mycolormap' );
@@ -76,10 +77,20 @@ function main()
         geometry.faces.push( face );
     }
 
-    material.vertexColors = THREE.FaceColors;
+    // Assign colors for each vertex
+    material.vertexColors = THREE.VertexColors;
     for ( var i = 0; i < nfaces; i++ )
     {
-        geometry.faces[i].color = new THREE.Color( 1, 1, 1 );
+        var id = faces[i];
+        var S0 = scalars[ id[0] ];
+        var S1 = scalars[ id[1] ];
+        var S2 = scalars[ id[2] ];
+        var C0 = new THREE.Color().setHex( cmap[ S0 ][1] );
+        var C1 = new THREE.Color().setHex( cmap[ S1 ][1] );
+        var C2 = new THREE.Color().setHex( cmap[ S2 ][1] );
+        geometry.faces[i].vertexColors.push( C0 );
+        geometry.faces[i].vertexColors.push( C1 );
+        geometry.faces[i].vertexColors.push( C2 );
     }
 
     var triangle = new THREE.Mesh( geometry, material );
@@ -93,4 +104,3 @@ function main()
         renderer.render( scene, camera );
     }
 }
-
